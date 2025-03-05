908f216d1d9b1d5774bc5a8f6d1a571f7288a818
ixsoumod <mplouka4@gmail.com>
2025-03-05 14:05:54 +0100

    Feat: /cc create <arena>

--- src/main/java/fr/ixsou/capturecore/commands/CaptureCoreAdmin.java
+++ src/main/java/fr/ixsou/capturecore/commands/CaptureCoreAdmin.java
@@ -55,7 +55,7 @@ public class CaptureCoreAdmin implements CommandExecutor {
 
             String nomarene = args[1]; // Récupérer l'argument 1
             Arene.add(nomarene); // Ajouter à la liste
-            plugin.getConfig().set("arenas", nomarene); // Sauvegarder la liste dans la configuration
+            plugin.getConfig().set("arenas", nomarene); // Sauvegarder la liste dans la configuratio
 
 
             plugin.saveConfig(); // Sauvegarder la configuration
